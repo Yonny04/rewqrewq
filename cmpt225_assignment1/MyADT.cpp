a6e5ee604fcb5732588c2be72b8c0ba84a7807fb
yonny <yoonyou0521@gmail.com>
2024-02-12 15:31:59 -0800

    Updated Feb.12 2023

--- cmpt225_assignment1/MyADT.cpp
+++ cmpt225_assignment1/MyADT.cpp
@@ -18,151 +18,62 @@
 #include <iostream>
 #include <cctype>
 #include "MyADT.h"     // Header file of MyADT file
-#include "Profile.h"   // Header file of Profile class
+#include "Profile.h"   // Header file of Profi le class
 
 using std::cout;
 using std::endl;
 
-/*
- * MyADT.h
- * 
- * Class Description: A linear data collection ADT.
- * 
- * Class Invariant: Data collection with the following characteristics:
- *                  - This is a value-oriented data collection and 
- *                    the elements are kept in ascending sort order of search key.
- *                  - Each element is unique (no duplicates). 
- *                  - Its data structure (CDT) is not expandable. This signifies that 
- *                    when this data collection becomes full, you do not have to resize 
- *                    its data structure (not in this Assignment 1). 
- *
- * Author: AL and <put your name here>
- * Last modified on: Jan. 2024
- */
- 
- 
-
-/*
- * You can add more #include statements, if needed.
- */
- 
-#include <string>
-// #include "Profile.h"
-
-
-class MyADT {
-
-private:
-
-/* 
- * You can add more data members (attributes) to this class, if needed,
- * but you cannot remove/change the data members below.
- * However, you can change the value "5" used to initialise MAX_ELEMENTS.
- */
- 
-    constexpr static unsigned int MAX_ALPHA = 26;   // 26 letters in the alphabet 
-    constexpr static unsigned int MAX_ELEMENTS = 5; // Small capacity so can test when data collection becomes full 
-                                                    // Feel free to change this MAX_ELEMENTS value.
-    Profile * elements[MAX_ALPHA];                  // MAX_ALPHA arrays of elements 
-    unsigned int elementCount[MAX_ALPHA];           // Current element count in each of the MAX_ALPHA arrays of elements
-
-
-/* 
- * You can add private methods to this class, if needed.
- */
- 
-    // Utility method(s)
-
-      
-public:
-
-/* 
- * You cannot remove/change the public methods below.
- * In other words, you cannot change the ***public interface*** of this class. 
- */
-
-    // Default constructor
-    MyADT();
-
-    // Copy constructor - Covered in Lab 3
-    MyADT(const MyADT& rhs);
-
-	// Overloaded assignment operator - Covered in Lab 5
-    // Therefore, we shall not be overloading this operator in our Assignment 1
-    // MyADT & operator=(const MyADT& rhs);
-    
-    // Destructor
-    // Description: Destroys this object, releasing heap-allocated memory.
-    ~MyADT();
-
-    // Description: Returns the total number of elements currently stored in the data collection MyADT.  
-    unsigned int getElementCount() const;
-
-    // Description: Inserts an element in the data collection MyADT.
-    // Precondition: newElement must not already be in the data collection MyADT.  
-    // Postcondition: newElement inserted and the appropriate elementCount has been incremented.
-    //                Returns "true" when the insertion is successfull, otherwise "false".
-    bool insert(const Profile& newElement);
-
-    // Description: Removes an element from the data collection MyADT. 
-    // Postcondition: toBeRemoved (if found) is removed and the appropriate elementCount is decremented.
-    //                Returns "true" when the removal is successfull, otherwise "false".    
-    bool remove(const Profile& toBeRemoved);
-	
-    // Description: Removes all elements from the data collection MyADT. 
-    // Postcondition: MyADT reverts back to its initialization state, i.e., 
-    //                the state it is in once it has been constructed (once
-    //                the default constructor has executed). 
-    void removeAll();
-   
-    // Description: Searches for target element in the data collection MyADT. 
-    //              Returns a pointer to the element if found, otherwise, returns nullptr.
-    Profile * search(const Profile& target);
-    
-    // Description: Prints all elements stored in the data collection MyADT in ascending order of search key.
-    // ***For Testing Purposes - Not part of this class' public interface.***
-    void print();
-   
-
-}; // end MyADT
-
-
 // Default constructor.
-MyADT::MyADT() { 
-    
-}  
+MyADT::MyADT() {
 
-// Copy constructor - Covered in Lab 3
-MyADT::MyADT(const MyADT& rhs){   
+    for (int i = 0; i < MAX_ALPHA; i++) {
+          elementCount[i] = 0;        // sets a default count
+          elements[i] = new Profile[MAX_ELEMENTS];
+            for (int j = 0; j < MAX_ELEMENTS; j++) {
+                 elements[i][j] = Profile();
+            }
+    }
+} 
 
-    // elements = rhs.element;
-    // elementCount = rhs.elementCount;
-    // element_arr = new string[elementCount];
-    // for (int i=0; i < getElementCount(); i++){
-    //     element_arr[i] = rhs.element_arr;
-    // }
-}  
+// Copy constructor
+MyADT::MyADT(const MyADT& rhs){
+
+    elementCount[MAX_ALPHA] = rhs.elementCount[MAX_ALPHA];  //copy
+        for (int i = 0; i < MAX_ALPHA; i++) {
+            elements[i] = new Profile[MAX_ELEMENTS];  
+                for (int j = 0; j < rhs.elementCount[i]; j++) {
+                    elements[i][j] = rhs.elements[i][j];  //copy
+            }
+        }
+
+}
     
 // Destructor
 // Description: Destroys this object, releasing heap-allocated memory.
 MyADT::~MyADT() {
-
-   //deletes all that is allocated at the pointer
-
-   for (int i=0; i<getElementCount(); i++){
-      delete[] elements[i];
+   for (int i=0; i < MAX_ALPHA; i++){
+      delete[] elements[i];  // deletes the element
    }
 }  
 
 
 // Description: Returns the total number of elements currently stored in the data collection MyADT.  
 unsigned int MyADT::getElementCount() const {
-   int sum; //variable to store the total amount of elements
+   int sum; // variable to store the total amount of elements
 
-   for (int i=0; i < getElementCount(); i++){
-      sum =+ elementCount[i]; //iterating through all the arrays and adding to sum
+   for (int i=0; i < MAX_ALPHA; i++){
+      sum += elementCount[i]; // iterating through all the arrays and adding to sum
    }
    return sum;
+}
+
+// Description: Convert a char value to int
+// Precondition: Input letter must be in lowercase
+// Postcondition: returns the ASCII value of letter 
+unsigned int MyADT::charToNum(Profile letter) {
+
+   int c = (letter.getUserName().at(0));  // get the letter at index 0 from profile letter
+   return (c - 'a');  // subtract it by the ASCII value of 'a' to return a int value
 }
 
 
@@ -171,42 +82,62 @@ unsigned int MyADT::getElementCount() const {
 // Postcondition: newElement inserted and the appropriate elementCount has been incremented.
 //                Returns "true" when the insertion is successfull, otherwise "false".
 bool MyADT::insert(const Profile& newElement) {
-   //see if the current element count is full or null
-      //if true
-         //I am going to push newElement into the pointer of that node
+   unsigned int index = charToNum(newElement);
 
+   if (elementCount[index] >= MAX_ELEMENTS) {   // to check if the elementCount is greater than the Max_Element
+      return false;    // if above condition is true, return false
+   }
+   for (unsigned int i = 0; i < elementCount[index]; ++i) {
+           if (elements[index][i] == newElement) { // Checking if element already exists, therefore cannot insert
+               return false;
+           }
+   }
 
-    return false;
+   unsigned int insertIndex = 0;
+   while (insertIndex < elementCount[index] && elements[index][insertIndex] < newElement) {  // edge case to insure insertIndex is less than elementCount AND elements is less than newElement
+       insertIndex++;
+   }
+
+   for (unsigned int i = elementCount[index]; i > insertIndex; --i) {
+       elements[index][i] = elements[index][i - 1];      // shifting elements in the array to the right
+   }
+
+   elements[index][insertIndex] = newElement;      // performs the insertion and increments the elementCount of the array
+   elementCount[index]++;
+
+   return true;
 }  
 
 
 // Description: Removes an element from the data collection MyADT. 
 // Postcondition: toBeRemoved (if found) is removed and the appropriate elementCount is decremented.
 //                Returns "true" when the removal is successfull, otherwise "false".  
+
 bool MyADT::remove(const Profile& toBeRemoved) {
+    bool check = false; // returns value of true or false
 
-    for (int i = 0; i < getElementCount(); i++) {
-        // Compare the search key of the current element with the search key of toBeRemoved
-        if (elements[i]->getSearchKey() == toBeRemoved.getSearchKey()) {
-            if (*elements[i] == toBeRemoved){
-               delete[] elements[i];
-            }
+       if (search(toBeRemoved) == nullptr) { // checks if toBeRemoved exists in data structure by calling the search method (below)
+          return check; // if its a nullptr (already remove/gone), return false value bc its not work
+       }
 
-            // filling the gap
-            for (int j = i; j < getElementCount() - 1; j++) {
-                elements[j] = elements[j + 1];
-            }
+       unsigned int index = charToNum(toBeRemoved); // gets num of first char of toBeRemoved
 
-            // Decrementing the element count
-            elementCount[i]--;
-            return true;  // Return true when removal is successful
-        }
-    }
+       for (unsigned int i = 0; i < elementCount[index]; i++)  // increments till first letter of toBeRemoved is reached
+       { 
+           if (elements[index][i] == toBeRemoved) { // checks if the current element is equal to the toBeRemoved profile
+             check = true;
+          }
 
-    // If no match is found, return false
-    return false;
+          if (check) { // if check is true
+             elements[index][i] = elements[index][i+1]; // shift the elements to fill the gap left by the removed element
+          }
+
+       }
+
+       elementCount[index]--; // decrement the count of elements at the calculated index
+       return check;
+
 }
-
 
 
 // Description: Removes all elements from the data collection MyADT. 
@@ -214,44 +145,38 @@ bool MyADT::remove(const Profile& toBeRemoved) {
 //                the state it is in once it has been constructed (once
 //                the default constructor has executed). 
 void MyADT::removeAll() {
-    
-    // for (int i; i < getElementCount(); i++){
-    //   delete[] elements[i]; //delete all the old arrays 
-    //   elements[i] = new Profile[MAX_ELEMENTS];  
-    // }
-    //deleting all dynamic memory and reset
-    //use the delete function
+   for (int i=0; i < MAX_ALPHA; i++){
+      elements[i] = nullptr;     //elements to null
+      elementCount[i] = 0; // resets the count
+   } 
 }   
 
 // Description: Searches for target element in the data collection MyADT. 
 //              Returns a pointer to the element if found, otherwise, returns nullptr.
-Profile* MyADT::search(const Profile& target) { 
-   
-   for (int i=0; i < getElementCount(); i++){         //iterate through to match element to target
-     
-         if (*elements[i] == target){  //seeing if the two matches
-            return elements[i];        //if found, return the pointer
-         }
-   }
-   //otherwise return nullptr
-   return nullptr;
-}  
+Profile* MyADT::search(const Profile& target) {
+    for (int i = 0; i < MAX_ALPHA; i++) {
+        for (int j = 0; j < elementCount[i]; j++) {
+            if (elements[i][j] == target) {
+                return &elements[i][j];  // Return the address of the found element
+            }
+        }
+    }
+    return nullptr;  // Return nullptr if not found
+}
+
 
 
 // Description: Prints all elements stored in the data collection MyADT in ascending order of search key.
 // ***For Testing Purposes - Not part of this class' public interface.***
 void MyADT::print() {
   
-    //each alphabet has a value
-    //see what bigger and print
-
-   // for(int i=0; i<getElementCount(); i++){
-
-        
-        
-
-
-    //}
+    for(int i = 0; i < MAX_ALPHA; i++) // letters
+	{
+		for(int j = 0; j < elementCount[i]; j++) // all elements of each letter
+		{
+			cout << elements[i][j].getUserName() << ", " << elements[i][j].getName() << ", " << elements[i][j].getEmail() << elements[i][j].getBirthday() << endl; // all the components of friends book
+		}
+	}
 } 
 
 //  End of implementation file
--- cmpt225_assignment1/MyADT.h
+++ cmpt225_assignment1/MyADT.h
@@ -1,28 +1,28 @@
 /*
  * MyADT.h
- * 
- * Class Description: A linear data collection ADT.
- * 
- * Class Invariant: Data collection with the following characteristics:
- *                  - This is a value-oriented data collection and 
- *                    the elements are kept in ascending sort order of search key.
- *                  - Each element is unique (no duplicates). 
- *                  - Its data structure (CDT) is not expandable. This signifies that 
- *                    when this data collection becomes full, you do not have to resize 
- *                    its data structure (not in this Assignment 1). 
  *
- * Author: AL and <put your name here>
- * Last modified on: Jan. 2024
+ * Class Description: A linear data collection ADT.
+ *
+ * Class Invariant: Data collection with the following characteristics:
+ *                  - This is a value-oriented data collection and
+ *                    the elements are kept in ascending sort order of search key.
+ *                  - Each element is unique (no duplicates).
+ *                  - Its data structure (CDT) is not expandable. This signifies that
+ *                    when this data collection becomes full, you do not have to resize
+ *                    its data structure (not in this Assignment 1).
+ *
+ * Author: AL and <Yoonsang You>
+ * Last modified on: Feb. 2 2024
  */
- 
- 
+
+
 #ifndef MyADT_H
 #define MyADT_H
 
 /*
  * You can add more #include statements, if needed.
  */
- 
+
 #include <string>
 #include "Profile.h"
 
@@ -31,31 +31,31 @@ class MyADT {
 
 private:
 
-/* 
+/*
  * You can add more data members (attributes) to this class, if needed,
  * but you cannot remove/change the data members below.
  * However, you can change the value "5" used to initialise MAX_ELEMENTS.
  */
- 
-    constexpr static unsigned int MAX_ALPHA = 26;   // 26 letters in the alphabet 
-    constexpr static unsigned int MAX_ELEMENTS = 5; // Small capacity so can test when data collection becomes full 
-                                                    // Feel free to change this MAX_ELEMENTS value.
-    Profile * elements[MAX_ALPHA];                  // MAX_ALPHA arrays of elements 
+
+    constexpr static unsigned int MAX_ALPHA = 26;   // 26 letters in the alphabet
+    constexpr static unsigned int MAX_ELEMENTS = 5; // Small capacity so can test when data collection becomes full
+
+    Profile * elements[MAX_ALPHA];                  // MAX_ALPHA arrays of elements that stores pointers
     unsigned int elementCount[MAX_ALPHA];           // Current element count in each of the MAX_ALPHA arrays of elements
 
 
-/* 
+/*
  * You can add private methods to this class, if needed.
  */
- 
-    // Utility method(s)
 
-      
+    // Utility method(s)
+    unsigned int charToNum(Profile letter);
+
 public:
 
-/* 
+/*
  * You cannot remove/change the public methods below.
- * In other words, you cannot change the ***public interface*** of this class. 
+ * In other words, you cannot change the ***public interface*** of this class.
  */
 
     // Default constructor
@@ -64,42 +64,38 @@ public:
     // Copy constructor - Covered in Lab 3
     MyADT(const MyADT& rhs);
 
-	// Overloaded assignment operator - Covered in Lab 5
-    // Therefore, we shall not be overloading this operator in our Assignment 1
-    // MyADT & operator=(const MyADT& rhs);
-    
     // Destructor
     // Description: Destroys this object, releasing heap-allocated memory.
     ~MyADT();
 
-    // Description: Returns the total number of elements currently stored in the data collection MyADT.  
+    // Description: Returns the total number of elements currently stored in the data collection MyADT.
     unsigned int getElementCount() const;
 
     // Description: Inserts an element in the data collection MyADT.
-    // Precondition: newElement must not already be in the data collection MyADT.  
+    // Precondition: newElement must not already be in the data collection MyADT.
     // Postcondition: newElement inserted and the appropriate elementCount has been incremented.
     //                Returns "true" when the insertion is successfull, otherwise "false".
     bool insert(const Profile& newElement);
 
-    // Description: Removes an element from the data collection MyADT. 
+    // Description: Removes an element from the data collection MyADT.
     // Postcondition: toBeRemoved (if found) is removed and the appropriate elementCount is decremented.
-    //                Returns "true" when the removal is successfull, otherwise "false".    
+    //                Returns "true" when the removal is successfull, otherwise "false".
     bool remove(const Profile& toBeRemoved);
-	
-    // Description: Removes all elements from the data collection MyADT. 
-    // Postcondition: MyADT reverts back to its initialization state, i.e., 
+
+    // Description: Removes all elements from the data collection MyADT.
+    // Postcondition: MyADT reverts back to its initialization state, i.e.,
     //                the state it is in once it has been constructed (once
-    //                the default constructor has executed). 
+    //                the default constructor has executed).
     void removeAll();
-   
-    // Description: Searches for target element in the data collection MyADT. 
+
+    // Description: Searches for target element in the data collection MyADT.
     //              Returns a pointer to the element if found, otherwise, returns nullptr.
     Profile * search(const Profile& target);
-    
+
     // Description: Prints all elements stored in the data collection MyADT in ascending order of search key.
     // ***For Testing Purposes - Not part of this class' public interface.***
     void print();
-   
+
 
 }; // end MyADT
 #endif
