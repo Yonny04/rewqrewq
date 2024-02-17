29165107db566cce33903819a6d05d5a7885d262
Yoon <Yoonyou0521@gmail.com>
2024-02-16 22:00:57 -0800

    ALL

--- cmpt225_Assignment2/Question3/Eval.cpp
+++ cmpt225_Assignment2/Question3/Eval.cpp
@@ -3,8 +3,8 @@
  *
  * Description: Evaluation of infix expressions using two Stacks.
  *
- * Author: Senka Kuboki
- * Date: 02/15/2024
+ * Author: Yoonsang You
+ * Date: Feb 16, 2024
  */
 
 #include <iostream>
@@ -15,17 +15,17 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-int perform(int i, int j, TokenType type) { //what the perform function does in each case
-   if (type == pltok) {
+int perform(int i, int j, TokenType typ) { //what the perform function does in each case
+   if (typ == pltok) {
       return i+j;
    }
-   else if (type == mitok) {
+   else if (typ == mitok) {
       return i-j;
    }
-   else if (type == asttok) {
+   else if (typ == asttok) {
       return i*j;
    }
-   else if (type == slashtok) {
+   else if (typ == slashtok) {
       return i/j;
    }
    else {
@@ -44,73 +44,72 @@ int main () {
 
     t = S.getnext();
 
-     while (t.tt != eof || !opstack.isEmpty()) { //while T is not EOF or the operator stack is non empty
-        if (t.tt == integer) { //if T is a number:
-           numstack.push(t); //push T to the number stack
-           t = S.getnext(); //get the next Token
+     while (t.tt != eof || !opstack.isEmpty()) {    //if it is empty
+        if (t.tt == integer) {
+           numstack.push(t); 
+           t = S.getnext(); 
         }
-        else if (t.tt == lptok) { //else if T is a left parenthesis
-           opstack.push(t); //push T to the operator stack
-           t = S.getnext(); //get the next token
+        else if (t.tt == lptok) {   //left parenthesis
+           opstack.push(t); 
+           t = S.getnext(); 
         }
-        else if (t.tt == rptok) { //else if T is a right parenthesis
-           if (opstack.peek().tt == lptok) { //if the top of the operator stack is a left parenthesis
-              opstack.pop(); //pop it from the operator stack
-              t = S.getnext(); //get the next token
+        else if (t.tt == rptok) {
+           if (opstack.peek().tt == lptok) {  //right parenthesis
+              opstack.pop();
+              t = S.getnext(); 
            }
            else {
-              Token result1; //create token for result to push at end
-              result1.tt = integer; //token type is integer
-              result1.val = perform(numstack.pop().val, numstack.pop().val, opstack.pop().tt);
-              //pop the top two numbers and the top operator and perform the operation
-              numstack.push(result1); //push the result to the number stack
+              Token result1; 
+              result1.tt = integer; 
+              result1.val = perform(numstack.pop().val, numstack.pop().val, opstack.pop().tt); //popping the two numbers and operators
+              
+              numstack.push(result1); //pushing as tokens
            }
         }
-        else if (t.tt == pltok || t.tt == mitok || t.tt == eof) { //else if T is +, - or EOF
-           if (!opstack.isEmpty()) {  //if the operator stack is nonempty
-              if (opstack.peek().tt == pltok || opstack.peek().tt == mitok || opstack.peek().tt == asttok || opstack.peek().tt == slashtok) {
-              //and the top is one of +, -, *, /
-                 Token result2; //create token for result to push at end
-                 result2.tt = integer; //token type is integer
+        else if (t.tt == pltok || t.tt == mitok || t.tt == eof) {       //+ or -  or EOF
+           if (!opstack.isEmpty()) {  
+              if (opstack.peek().tt == pltok || opstack.peek().tt == mitok || opstack.peek().tt == asttok || opstack.peek().tt == slashtok) {  //implementing peeking and comparing
+              
+                 Token result2; 
+                 result2.tt = integer; 
                  result2.val = perform(numstack.pop().val, numstack.pop().val, opstack.pop().tt);
-                 // pop the top two numbers and the top operator and perform the operation
-                 numstack.push(result2); //push the result to the number stack
+                
+                 numstack.push(result2); 
               }
               else {
                  opstack.push(t); //push T to the operator stack
-                 t = S.getnext(); //get the next token
+                 t = S.getnext(); 
               }
            }
            else {
               opstack.push(t); //push T to the operator stack
-              t = S.getnext(); //get the next token
+              t = S.getnext(); 
            }
         }
-        else if (t.tt == asttok || t.tt == slashtok) { //else if T is * or /
-           if (!opstack.isEmpty()) { //if the operator stack is nonempty
-              if (opstack.peek().tt == asttok || opstack.peek().tt == slashtok) { //and the top is one of *, /:
-                 Token result3; //create token for result to push at end
-                 result3.tt = integer; //token type is integer
+        else if (t.tt == asttok || t.tt == slashtok) { //for * and /
+           if (!opstack.isEmpty()) { 
+              if (opstack.peek().tt == asttok || opstack.peek().tt == slashtok) { 
+                 Token result3; 
+                 result3.tt = integer; 
                  result3.val = perform(numstack.pop().val, numstack.pop().val, opstack.pop().tt);
-                 // pop the top two numbers and the top operator and perform the operation
+                 
                  numstack.push(result3); //push the result to the number stack
               }
               else {
                  opstack.push(t); //push T to the operator stack
-                 t = S.getnext(); //get the next token
+                 t = S.getnext(); 
               }
            }
            else {
               opstack.push(t); //push T to the operator stack
-              t = S.getnext(); //get the next token
+              t = S.getnext();
            }
         }
-     } //end of while loop. equation is done
+     } 
 
-    int resultfinal; //print the final answer to the equation.
+    int resultfinal; //printing all of what was calculated
     resultfinal = numstack.pop().val;
     cout << resultfinal << endl;
-    // End pretty printer coding demo.
-
+    //End pretty printer demo.
     return 0;
 }
